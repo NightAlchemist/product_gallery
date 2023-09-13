7168201587026c8776cb1e99a5c86b4870e39e2a
Jhoao Fajardo <cjam02@hotmail.com>
2023-09-13 16:01:52 -0500

    factory corrected

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,14 +22,17 @@ class ProductFactory extends Factory
     {
 
         return [
-            'title' => $this->faker->randomElement(['Nike Shirt', 'Nike Skirt', 'Nike Shoes', 'Nike Sweatpants']),
-            'description' => $this->faker->paragraph(3),
-            'price' => $this->faker->randomFloat(2,100,150),
-            'price_after_discount' => $this->faker->randomFloat(2,50,99),
-            'reviews' => $this->faker->numberBetween(21,145),
-            'total_sales' => $this->faker->numberBetween(15,480),
-            'stock' => $this->faker->numberBetween(0,80),
-            'tags' => $this->faker->randomElements(['clothes', 'wearable', 'accessories', 'shoes', 'fashion']),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'price_after_discount' => $this->faker->optional(0.5)->randomFloat(2, 5, 500),
+            'reviews' => $this->faker->numberBetween(0, 100),
+            'total_sales' => $this->faker->numberBetween(0, 1000),
+            'stock' => $this->faker->numberBetween(0, 100),
+            'tags' => $this->faker->words(3, true),
+            'image_path' => $this->faker->imageUrl(600, 400, 'products', true),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
